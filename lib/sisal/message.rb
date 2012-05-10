679679403765639d658bc29b8c29c4c9af87dd29
Vinicius Horewicz <vinicius@horewi.cz>
2012-05-10 17:11:56 -0300

    Message handle one recipient and is unopiniated about encoding
    Encoding and message size checks will be done by encoding classes in
    the near future.

--- lib/sisal/message.rb
+++ lib/sisal/message.rb
@@ -1,12 +1,9 @@
 module Sisal
   class Message
     InvalidPhoneNumber  = Class.new(StandardError)
-    MessageTooLarge     = Class.new(StandardError)
-    InvalidEncoding     = Class.new(StandardError)
 
     PHONE_REGEX       = /^\d+$/
     PHONE_LENGTH      = [4, 15]
-    SMS_MAX_SIZE      = 140
 
     attr_accessor :to, :text
 
@@ -14,31 +11,15 @@ module Sisal
       @to, @text = to, text
     end
 
-    def text
-      @text.encode('US-ASCII')
-    end
-
     def valid?
-      validate_numbers
-      validate_encoding
-      validate_message_size
+      validate_number
     end
 
-    def validate_numbers
-      to.each { |number| valid_number? number }
-    end
+    private
 
-    def valid_number?(number)
-      number.length.between?(*PHONE_LENGTH) && number.match(PHONE_REGEX) or
-      raise InvalidPhoneNumber.new(number)
-    end
-
-    def validate_message_size
-      raise MessageTooLarge unless @text.bytesize <= SMS_MAX_SIZE
-    end
-
-    def validate_encoding
-      raise InvalidEncoding unless @text.encoding.ascii_compatible?
+    def validate_number
+      to.length.between?(*PHONE_LENGTH) && to.match(PHONE_REGEX) or
+      raise InvalidPhoneNumber.new(to)
     end
   end
 end
--- spec/sisal/message_spec.rb
+++ spec/sisal/message_spec.rb
@@ -1,79 +1,33 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Sisal::Message do
-  let(:message) { Sisal::Message.new(%w(5525800 5525801), '') }
-
-  describe "#text" do
-    it "encodes text to us-ascii" do
-      str = 'Hello MU!'
-      str.should_receive(:encode).with('US-ASCII')
-      message.text = str
-      message.text
-    end
-  end
-
-  describe "#to" do
-    it "normalizes recipients list to array" do
-      message.to = '5525800'
-      message.to.should eq(['5525800'])
-    end
-
-    it "flatten the recipients list" do
-      message.to.should have(2).items
-    end
-  end
+  let(:message) { Sisal::Message.new("5525800", "") }
 
   describe "#valid?" do
-    it "validates numbers and message size" do
-      message.should_receive :validate_numbers
-      message.should_receive :validate_encoding
-      message.should_receive :validate_message_size
-      message.valid?
-    end
-  end
-
-  describe "#validate_numbers" do
-    it "validates each number" do
-      message.should_receive(:valid_number?).twice
-      message.validate_numbers
-    end
-  end
-
-  describe "#valid_number?" do
     context "raises an error when" do
-      it "has less than 4 digits" do
-        expect { message.valid_number?('123') }.to raise_error(Sisal::Message::InvalidPhoneNumber)
+      it "number has less than 4 digits" do
+        message.to = "123"
+        expect { message.valid? }.to raise_error(Sisal::Message::InvalidPhoneNumber)
       end
 
-      it "has more than 15 digits" do
-        expect { message.valid_number?('1' * 20) }.to raise_error(Sisal::Message::InvalidPhoneNumber)
+      it "number has more than 15 digits" do
+        message.to = "1" * 20
+        expect { message.valid? }.to raise_error(Sisal::Message::InvalidPhoneNumber)
       end
 
-      it "has chars other than digits" do
-        expect { message.valid_number?('+5525800') }.to raise_error(Sisal::Message::InvalidPhoneNumber)
+      it "number has chars other than digits" do
+        message.to = "+5525800"
+        expect { message.valid? }.to raise_error(Sisal::Message::InvalidPhoneNumber)
       end
     end
 
     it "accepts short codes" do
-      message.valid_number?('5525800').should be_true
+      message.valid?.should be_true
     end
 
     it "accepts complete E.164 number" do
-      message.valid_number?('230234321324213')
-    end
-  end
-
-  describe "#validate_message_size" do
-    it "raises an error when message larger than 140 chars" do
-      message.text = 'KTHXBYE' * 50
-      expect { message.validate_message_size }.to raise_error(Sisal::Message::MessageTooLarge)
-    end
-  end
-
-  describe "#validate_encoding" do
-    it "raises an error when not compatible with ASCII" do
-      message.text = 'Hello MU!'.force_encoding('UTF-16BE')
-      expect { message.validate_encoding }.to raise_error(Sisal::Message::InvalidEncoding)
+      message.to = "230234321324213"
+      message.valid?
     end
   end
 end
